39fe539a704136f32402fa2145f28db68b9fca6c
Raymond Haring <hari5580@mylaurier.ca>
2025-03-20 19:40:43 -0400

    Update logout.php, error_log.txt, and remove index.php

--- Project/logout.php
+++ Project/logout.php
@@ -1,17 +1,14 @@
 <?php
-
-declare(strict_types=1);
-
 // Check if the user is logged in
-if (isset($_COOKIE['user'])) {
+if (isset($_COOKIE['user_id'])) {
     // Remove the cookies
-    setcookie('user', '', time() - 3600);
-    setcookie('id', '', time() - 3600);
-    setcookie('firstName', '', time() - 3600);
-    setcookie('lastName', '', time() - 3600);
+    setcookie('user_id', '', time() - 3600);
+    setcookie("username", time() - 3600);
+    setcookie("email", time() - 3600);
+    setcookie('first_name', '', time() - 3600);
+    setcookie('last_name', '', time() - 3600);
+    header('Location: login.php');
+    exit();
 }
-
-// Redirect to the login page
-echo '<script> alert("You have successfully logged out."); window.location="login.php"</script>';
 
 ?>
